deebdc51cb1d3f55f97a756e54beaf67457cfe02
veetors <veetor.s@gmail.com>
2019-01-06 20:19:19 +0300

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,45 @@
+// const binarySearch = ( items, item, low = 0, high = items.length - 1 ) => {
+//     if ( low > high ) {
+//         return null;
+//     }
+
+//     const mid = Math.ceil( ( low + high ) / 2 );
+//     const guess = items[mid];
+
+//     if ( item === guess ) {
+//         return mid;
+//     }
+
+//     const newLow  = guess > item ? low : mid + 1;
+//     const newHigh = guess > item ? mid - 1 : high;
+
+//     return binarySearch( items, item, newLow, newHigh );
+// };
+
+const binarySearch = ( items, item ) => {
+    let low = 0;
+    let high = items.length - 1;
+
+    while ( low <= high ) {
+        const mid = Math.ceil( ( low + high ) / 2 );
+        const guess = items[mid];
+
+        if ( guess === item ) {
+            return mid;
+        }
+
+        if ( guess > item ) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    return null;
+};
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // eslint-disable-line
+const item = 5;
+
+binarySearch( list, item ); // => 4
