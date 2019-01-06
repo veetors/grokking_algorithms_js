ce0bf17add806b9028a0c942f00fd84b8894ba2d
veetors <veetor.s@gmail.com>
2019-01-06 20:19:57 +0300

    selectionSort

---
+++ selectionSort.js
@@ -0,0 +1,42 @@
+// const findSmallest = ( arr ) => {
+//     let smallest = arr[0];
+//     let smallestIndex = 0;
+
+//     arr.forEach( ( item, index ) => { // eslint-disable-line
+//         if ( item < smallest ) {
+//             smallest = item;
+//             smallestIndex = index;
+//         }
+//     } );
+
+//     return smallestIndex;
+// };
+
+const findSmallest = ( arr ) => {
+    if ( arr.length === 0 ) {
+        return arr;
+    }
+
+    const smallestEl = arr.reduce( ( prev, current ) => {
+        return prev < current ? prev : current;
+    } );
+
+    return arr.indexOf( smallestEl );
+};
+
+const selectionSort = ( arr ) => {
+    const iter = ( list, sortingList = [] ) => {
+        if ( list.length === 0 ) {
+            return sortingList;
+        }
+
+        const smallestIndex = findSmallest( list );
+        const current = list.splice( smallestIndex, 1 );
+
+        return iter( list, sortingList.concat( current ) );
+    };
+
+    return iter( arr );
+};
+
+selectionSort( [100, 0, 1, 3, -1, 0 ] ); // => [ -1, 0, 0, 1, 3, 100 ]
