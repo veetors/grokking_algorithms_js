de9eae406fe4f7dbf95e28235e1b5b1102ceeff9
veetors <veetor.s@gmail.com>
2019-01-06 19:56:47 +0300

    add dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', '@regru/regru'],
+  plugins: [
+    'promise',
+    'import'
+  ],
+  globals: {
+    '__statics': true
+  },
+  'rules': {
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
