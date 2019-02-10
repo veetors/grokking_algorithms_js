423a46f90d9d6f2b968c0fca9ab1d99fb2083b0c
veetors <veetor.s@gmail.com>
2019-02-10 20:59:11 +0300

    minor

--- __tests__/01_binarySearch.test.js
+++ __tests__/01_binarySearch.test.js
@@ -1,7 +1,8 @@
 import binarySearch from '../01_binarySearch';
 
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //eslint-disable-line
+
 test( 'binarySearch', () => {
-    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //eslint-disable-line
     const item = 5;
     const index = binarySearch( list, item );
 
@@ -9,7 +10,6 @@ test( 'binarySearch', () => {
 } );
 
 test( 'binarySearch null', () => {
-    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //eslint-disable-line
     const item = 20;
     const index = binarySearch( list, item );
 
